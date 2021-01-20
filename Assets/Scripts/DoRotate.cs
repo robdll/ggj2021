ddaba6dc464ae04c863544e6856f7486f1a1bb82
Bartolomeo Nardi <nardinardi85@gmail.com>
2021-01-20 10:58:35 +0100

    UI and Folders setup
    Crated Project Assets' folders, a prefab for the UI buttons, a MainMenu scene and a Game scene

---
+++ Assets/Scripts/DoRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoRotate : MonoBehaviour
+{
+    public float accelx, accely, accelz;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(accelx * Time.unscaledDeltaTime, accely * Time.unscaledDeltaTime, accelz * Time.unscaledDeltaTime);
+    }
+}
