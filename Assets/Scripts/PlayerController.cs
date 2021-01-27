80ebb14f71a05eadce8e3cd60f5d5c3daef297bc
Bartolomeo Nardi <nardinardi85@gmail.com>
2021-01-27 13:05:29 +0100

    New Scenes, Managers and Controllers
    Testing player death

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[RequireComponent(typeof(HealthController))]
+public class PlayerController : MonoBehaviour
+{
+    public int score = 0;
+    public int frags = 0;
+    public int assists = 0;
+    public delegate void OnPlayerDeathDelegate();
+    public event OnPlayerDeathDelegate deathEvent;
+    private HealthController healthController;
+    private void Start() 
+    {
+        
+        healthController = GetComponent<HealthController>();
+        /*no checks needed because each and every player shoud have a HealthController attached to it*/
+        healthController = healthController == null ?  healthController = gameObject.AddComponent<HealthController>() : healthController = this.healthController;
+    }
+
+    void Update() 
+    {
+        if(this.healthController.lives <= 0)
+        {
+            Death();
+        }
+    }
+    private void Death() 
+    {
+        if(deathEvent != null)
+        {
+            deathEvent();
+        }
+    }
+    
+}
---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{   
+    private void OnCollisionEnter(Collision other) /*AT LEAST ONE OBJECT HAS TO HAVE A RIGIDBODY COMPONENT TO MAKE THIS FUNCTION WORK*/
+    {
+        if(other != null)/*Just to be super sure :P */
+        {            
+            GameObject collidedObject = other.gameObject;
+            if(collidedObject.GetComponent<HealthController>() != null)
+            {                
+              //  collidedObject.onLifeGained += OneUp;
+                Debug.Log("OneUp!");
+                Destroy(this.gameObject);                
+            }
+            if(collidedObject.GetComponent<PlayerController>() != null)
+            {                
+                Debug.Log("Powerup Gained!");
+                Destroy(this.gameObject);
+            }
+        }    
+    }
+
+    public void OneUp(HealthController _healthController)
+    {
+        _healthController.lives++;
+    }
+}
