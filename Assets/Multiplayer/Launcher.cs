1686eab96b5b38f3b430daf07064135da8b2c887
koop4 <dilillo.roberto@gmail.com>
2021-01-30 04:58:49 +0000

    basic connect panel with room connection

---
+++ Assets/Multiplayer/Launcher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Connecting 2 Master");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected 2 Master");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        MenuManager.Instance.OpenMenu("connect");
+        Debug.Log("Lobby Joined");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
