7efdf0db1674732a629f3a438b8fe17ddcfb6783
Roberto Di Lillo <dilillo.roberto@gmail.com>
2021-01-30 16:45:42 +0000

    Room manager (#9)
    * instantiate player  in game scene
    
    * add player manager
    
    * adds player to scene

---
+++ Assets/Multiplayer/Scripts/PlayerManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+
+public class PlayerManager : MonoBehaviour
+{
+    PhotonView PV;
+
+    void Awake()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(PV.IsMine)
+        {
+            CreateController();
+        }
+    }
+
+    void CreateController()
+    {
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), Vector3.zero, Quaternion.identity);
+        Debug.Log("init controller here");
+    }
+}
---
+++ Assets/Multiplayer/Scripts/RoomManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    public static RoomManager Instance;
+
+    private void Awake()
+    {
+        if(Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+        Instance = this;
+    }
+
+    public override void OnEnable()
+    {
+        base.OnEnable();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    public void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    {
+
+        // In Game Scene we build a Room manager prefab.
+        // PhotonPrefabs must be in the resources older
+        // unity excludes any file not referenced in the editor from the final build
+        if(scene.buildIndex == 1)
+        {
+            PhotonNetwork.Instantiate( Path.Combine("PhotonPrefabs", "PlayerManager"), Vector3.zero, Quaternion.identity );
+        }
+    }
+
+}
