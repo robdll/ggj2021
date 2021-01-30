567875ce806021d58e0816149c50f952c9e76c6b
koop4 <dilillo.roberto@gmail.com>
2021-01-30 16:32:37 +0000

    add player manager

---
+++ Assets/Multiplayer/Scripts/PlayerManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerManager : MonoBehaviour
+{
+    PhotonView PV;
+
+    void Awake()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(PV.IsMine)
+        {
+            CreateController();
+        }
+    }
+
+    void CreateController()
+    {
+        Debug.Log("init controller here");
+            //init player controller here
+    }
+}
