9307d0cf496bf98cc49e94aaa0f5baaf2ffe4994
Roberto Di Lillo <dilillo.roberto@gmail.com>
2021-01-30 08:59:00 +0000

    Multiplayer (#5)
    * remove github plugin
    
    * basic connect panel with room connection
    
    * server room
    
    * find room menu
    
    * list hosted games
    
    * display players
    
    * not sure

---
+++ Assets/Multiplayer/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public string menuName;
+    public bool open;
+
+    // Start is called before the first frame update
+    public void Open()
+    {
+        open = true;
+        gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    public void Close()
+    {
+        open = false;
+        gameObject.SetActive(false);
+    }
+}
