c99e8a6c239ed4ae5b2f4ae0a98af178274d8a9c
koop4 <dilillo.roberto@gmail.com>
2021-01-31 06:07:02 +0000

    class abstraction

---
+++ Assets/Scripts/Eye.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Eye : Ability
+{
+    public abstract override void Use();
+}
---
+++ Assets/Scripts/SingleShotEye.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleShotEye : Eye
+{
+    public override void Use()
+    {
+        Debug.Log("Using eyes" + playerabilityInfo.abilityName );
+    }
+}
+
