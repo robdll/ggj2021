401661428aff7fd3f38642a3560b58ce0c651a6d
Bartolomeo Nardi <nardinardi85@gmail.com>
2021-01-31 06:16:55 +0100

    movement and animation fixes

--- Assets/Scripts/GroundCheck.cs
+++ Assets/Scripts/GroundCheck.cs
@@ -4,65 +4,82 @@ using UnityEngine;
 
 public class GroundCheck : MonoBehaviour
 {
-    PlayerController playerController;
-    void Awake()
-    {
-        playerController = GetComponentInParent<PlayerController>();
-    }
+    public PlayerController playerController;
+
 
     public void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject == playerController.gameObject)
+        if(other != null)
         {
-            return;
+            if (other.gameObject == playerController.gameObject)
+            {
+                return;
+            }
+            playerController.SetGroundedState(true);
         }
-        playerController.SetGroundedState(true);
     }
 
     public void OnTriggerExit(Collider other)
     {
-        if (other.gameObject == playerController.gameObject)
+        if (other != null)
         {
-            return;
+            if (other.gameObject == playerController.gameObject)
+            {
+                return;
+            }
+            playerController.SetGroundedState(false);
+
         }
-        playerController.SetGroundedState(false);
     }
 
     public void OnTriggerStay(Collider other)
     {
-        if (other.gameObject == playerController.gameObject)
+        if (other != null)
         {
-            return;
+            if (other.gameObject == playerController.gameObject)
+            {
+                return;
+            }
+
+            playerController.SetGroundedState(true);
         }
-        playerController.SetGroundedState(true);
     }
 
     public void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject == playerController.gameObject)
+        if(collision != null)
         {
-            return;
+            if (collision.gameObject == playerController.gameObject)
+            {
+                return;
+            }
+            playerController.SetGroundedState(true);
         }
-        playerController.SetGroundedState(true);
     }
 
     public void OnCollisionExit(Collision collision)
     {
-        if (collision.gameObject == playerController.gameObject)
+        if (collision != null)
         {
-            return;
+            if (collision.gameObject == playerController.gameObject)
+            {
+                return;
+            }
+            playerController.SetGroundedState(false);
         }
-        playerController.SetGroundedState(false);
     }
 
     public void OnCollisionStay(Collision collision)
     {
-        if (collision.gameObject == playerController.gameObject)
+        if (collision != null)
         {
-            return;
-        }
-        playerController.SetGroundedState(true);
-    }
+            if (collision.gameObject == playerController.gameObject)
+            {
+                return;
+            }
+            playerController.SetGroundedState(true);
 
+        }
+    }
 }
 
